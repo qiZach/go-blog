e12847821048d7247a5e1da6c52a1b2067baf46d
ZhangSiQi <1098795322@qq.com>
2022-08-09 17:49:01 +0800

    配置主页返回Html

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type IndexData struct {
+	Title string `json:"title"`
+	Desc  string `json:"desc"`
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var indexData IndexData
+	indexData.Title = "码神之路开启，Go Blog"
+	indexData.Desc = "盘古开天辟地"
+	bytes, _ := json.Marshal(indexData)
+	w.Write(bytes)
+}
+
+func indexHtml(w http.ResponseWriter, r *http.Request) {
+	var indexData IndexData
+	indexData.Title = "码神之路开启，Go Blog"
+	indexData.Desc = "盘古开天辟地"
+	t := template.New("index.html")
+	path, _ := os.Getwd()
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	t.Execute(w, indexData)
+}
+
 func main() {
 
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", indexHtml)
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
 }
